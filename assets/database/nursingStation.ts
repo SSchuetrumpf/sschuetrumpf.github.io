a0e0433252b7cbd2e5fcc7307dd1415ba5d6f2e6
Stephen Schuetrumpf <sschuetrumpf@gmail.com>
2020-11-10 01:07:45 -0600

    updated

---
+++ assets/database/nursingStation.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    userId: 2115,
+    nursingStationId: 'PEDS'
+  },
+  {
+    userId: 2115,
+    nursingStationId: 'EMR'
+  },
+  {
+    userId: 2115,
+    nursingStationId: 'SURG'
+  },
+  {
+    userId: 2,
+    nursingStationId: 'SURG'
+  }
+];
---
+++ assets/database/user.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    username: 'nurse',
+    password: 1234,
+    userid: 2115,
+    lastName: 'doe',
+    firstName: 'jane',
+    url: ''
+  },
+  {
+    username: 'assistant',
+    password: 1234,
+    userid: 1211,
+    lastName: 'doh',
+    firstName: 'john',
+    url: ''
+  }
+];
